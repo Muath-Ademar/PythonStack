f4f71558dd57e5fc98223ab1fdc2145e91c543cf
Muath-Ademar <muazademar20@gmail.com>
2024-04-28 16:07:57 +0300

    Assinments

---
+++ _python/python_fundementals/for_loop_basic1.py
@@ -0,0 +1,35 @@
+for x in range(151):
+    print(x)
+
+for x in range(5, 1005):
+    if x%5==0:
+        print(x)
+
+for x in range(1, 101):
+    if x%5 == 0:
+        print("coding")
+
+    if x%10 == 0: 
+        print ("coding dojo")
+
+    else:
+        print(x)
+
+final_sum = 0
+for x in range(0, 500001, 2):
+    final_sum= final_sum+x
+    print(final_sum)
+    
+
+
+for x in range(2018,0,-4):           
+    print(x)   
+
+lowNum=2
+highNum=9
+mult=3
+for x in range(lowNum, highNum + 1):
+    if x%mult == 0:
+        print(x)
+
+
