12aba7490813e2ab6cebace8b875c6f758ff717e
Muath-Ademar <muazademar20@gmail.com>
2024-04-30 15:27:30 +0300

    chaning

--- _python/OOP/User.py
+++ _python/OOP/User.py
@@ -5,29 +5,18 @@ class User:
         self.balance = 0
     def makedeposit(self, amount):
         self.balance= self.balance+amount
+        return self
     def makeWhithdrawal(self, amount):
         self.balance= self.balance-amount
+        return self
     def displayUserbalance(self, amount):
         print(self.balance)
+        return self
 
 mike = User("mike", "mike@gmail.com")
 muath = User("muath", "muath@gmail.com")
 Tony = User("Tony", "Tony@gmail.com")
 
-mike.makedeposit(200)
-mike.makedeposit(200)
-mike.makedeposit(200)
-mike.makeWhithdrawal(100)
-mike.displayUserbalance("amount")
-
-muath.makedeposit(200)
-muath.makedeposit(200)
-muath.makeWhithdrawal(100)
-muath.makeWhithdrawal(50)
-muath.displayUserbalance("amount")
-
-Tony.makedeposit(1000)
-Tony.makeWhithdrawal(200)
-Tony.makeWhithdrawal(200)
-Tony.makeWhithdrawal(200)
-Tony.displayUserbalance("amount")
+mike.makedeposit(200).makedeposit(200).makedeposit(200).makeWhithdrawal(100).displayUserbalance("amount")
+muath.makedeposit(200).makedeposit(200).makeWhithdrawal(100).makeWhithdrawal(50).displayUserbalance("amount")
+Tony.makedeposit(1000).makeWhithdrawal(200).makeWhithdrawal(200).makeWhithdrawal(200).displayUserbalance("amount")
