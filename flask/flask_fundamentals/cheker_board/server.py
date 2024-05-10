b9cd877154f410f89954d1eec4918de6af7eebf0
Muath-Ademar <muazademar20@gmail.com>
2024-05-10 17:19:45 +0300

    checker with bouneses

--- flask/flask_fundamentals/cheker_board/server.py
+++ flask/flask_fundamentals/cheker_board/server.py
@@ -5,10 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def user():
-    return render_template ("index.html", num = 4) 
+    return render_template ("index.html", num = 4 , num2=4) 
 
 @app.route("/<num>")
 def user2(num):
-    return render_template("index.html", num = int(num))
+    return render_template("index.html", num = int(num) ,num2=int(num))
+@app.route("/<num>/<num2>")
+def user3(num, num2):
+    return render_template("index.html", num= int(num), num2 = int(num2))
+@app.route("/<num>/<num2>/<color1>/<color2>")
+def color(num, num2, color1, color2):
+    return render_template("index.html", num= int(num), num2 = int(num2), color1 = color1, color2 = color2)
+
 if __name__ =="__main__":
     app.run(debug=True)
